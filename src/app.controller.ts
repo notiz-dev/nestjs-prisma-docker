aa3b6a1b700b95831002b5d8b0e651338993c9da
Marc Stammerjohann <marcstammerjohann@web.de>
2020-07-29 10:24:07 +0200

    prepare simple prisma schema

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,22 @@
+import { PrismaService } from './prisma/prisma.service';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Food } from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly prismaService: PrismaService,
+  ) {}
 
   @Get()
   getHello(): string {
     return this.appService.getHello();
+  }
+
+  @Get('foods')
+  getFoods(): Promise<Food[]> {
+    return this.prismaService.food.findMany();
   }
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { PrismaService } from './prisma/prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
